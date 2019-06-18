8696c7eb10f9559a21a7144f641da516e2a12f4e
Kenneth Muchiri <kenmucciz8@gmail.com>
2019-06-18 12:18:43 +0300

    initalized the test subclass and the self setup

--- user_test.py
+++ user_test.py
@@ -1,3 +1,18 @@
 import unittest # Importing the unittest module
 from password import User # Importing the user class
 from credentials import Credentials
+
+
+class TestUser(unittest.TestCase):
+    '''
+    Test class that defines test cases for the user class behaviours.
+
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases
+    '''
+
+    def setUp(self):
+        '''
+        Set up method to run before each test cases.
+        '''
+        self.new_user = User('1234','cartelo','kimonyoski') #create a new user object
