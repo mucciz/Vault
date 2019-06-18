e1ab0173d050c6385c1b7f5a80ba080cc66e4f1e
Kenneth Muchiri <kenmucciz8@gmail.com>
2019-06-18 12:07:18 +0300

    inititalized the init method

--- password.py
+++ password.py
@@ -4,3 +4,19 @@ class User:
     '''
 
     user_list = [] #Empty user list
+    def __init__(self,id_number, user_name, password):
+
+        '''
+        __init__ method that helps us define properties for our objects.
+
+        Args:
+        id_number: New user id_number.
+        user_name : New user username.
+        password: New user password.
+        '''
+        
+        self.id_number = id_number
+        self.user_name = user_name
+        self.password = password
+
+    
