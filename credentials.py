51821079914bf2a94440e512a18576034d61035d
Kenneth Muchiri <kenmucciz8@gmail.com>
2019-06-18 12:25:04 +0300

    intialize the credentials class

--- credentials.py
+++ credentials.py
@@ -1,1 +1,13 @@
 import secrets
+
+class Credentials:
+    '''
+    Class that generates new instances of credentials
+    '''
+    credentials_list = [] #Empty user list
+
+    def __init__(self,account, u_name, p_word):
+
+        self.account = account
+        self.u_name = u_name
+        self.p_word = p_word
